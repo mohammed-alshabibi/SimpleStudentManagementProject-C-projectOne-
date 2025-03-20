e12e5f9eac2ec82b13c365d5907551273754267c
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-03-20 10:08:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleStudentManagementProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
